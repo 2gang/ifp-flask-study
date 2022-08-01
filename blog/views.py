4ce090b1f86b4d5e85b46d04713bc0045c7c591c
이경호 <rud0503@gmail.com>
2022-08-01 18:25:54 +0900

    Update views.py

--- blog/views.py
+++ blog/views.py
@@ -1,5 +1,7 @@
+from sre_parse import CATEGORIES
 from flask import Blueprint, render_template
-from flask_login import current_user
+from flask_login import current_user, login_required
+from .models import get_category_model
 views = Blueprint("views", __name__)
 
 @views.route('/')
@@ -13,7 +15,9 @@ def about():
 
 @views.route('/categories-list')
 def categories_list():
-    return render_template("categories_list.html", user=current_user)
+    categories = get_category_model().query.all() #모든 카테고리를 가져오고,
+    #아래에서 context로 추가해 준다.
+    return render_template("categories_list.html", user=current_user, categories=categories)
 
 @views.route("/post-list")
 def post_list():
@@ -26,3 +30,7 @@ def post_detail():
 @views.route('/contact')
 def contact():
     return render_template("contact.html", user=current_user)
+
+@views.route("/create-post")
+def create_post():
+    return render_template("post_create_form.html", user=current_user)
