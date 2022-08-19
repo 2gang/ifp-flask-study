6cf74255917e57b6a2a118c91a30a35653a2a3b5
이경호 <rud0503@gmail.com>
2022-08-19 14:06:16 +0900

    test http api build

--- app.py
+++ app.py
@@ -1,7 +1,11 @@
-from flask import Flask, jsonify
+import re
+from urllib import request
+from flask import Flask, jsonify, request
 
 app = Flask(__name__)
 
+app.config['JSON_AS_ASCII'] = False
+app.config['JSONIFY_PRETTYPRINT_REGULAR'] = True
 
 shop_list = [
     {
@@ -13,7 +17,16 @@ shop_list = [
             }
         ]
     },
+    {
+        "items" : [],
+        "name" : "김밥지옥"
+    },
+    {
+        "items" : [],
+        "name" : "다없소"
+    }
 ]
+
 '''
 서버의 입장에서,
 POST -  클라이언트가 보낸 데이터를 받는 데에 사용한다.
@@ -25,27 +38,50 @@ GET  -  데이터를 다시 돌려주는 데에만 사용한다.
 # POST /shop -> name 이라는 데이터를 받아서, 새로운 shop을 생성함
 @app.route('/shop', methods=['POST'])
 def create_shop():
-    pass
+    request_data = request.get_json()
+    print(request_data)
+    print(type(request_data))
+    new_shop = {
+        'name' : request_data['name'],
+        'items' : []
+    }
+    shop_list.append(new_shop)
+    return jsonify(new_shop)
 
 # GET  /shop/<string:shop_name> -> name 에 맞는 shop 을 보여줌
 @app.route('/shop/<string:shop_name>', methods=['GET'])
-def get_shop_detail():
-    return jsonify(shop_list)
+def get_shop_detail(shop_name):
+    for shop in shop_list:
+        if shop['name'] == shop_name:
+            return jsonify(shop)
+    return jsonify({'message' : 'shop not found'})
 
 # GET  /shop -> shop의 목록들을 보여줌
 @app.route('/shop', methods=['GET'])
 def get_shop_list():
-    pass
+    return jsonify({'shop_list':shop_list})
 
 # POST /shop/<string:shop_name>/item -> item_name, price 를 받아서, name에 맞는 shop에 새로운 item을 추가
 @app.route('/shop/<string:shop_name>/item', methods=['POST'])
-def create_item_in_shop():
-    pass
+def create_item_in_shop(shop_name):
+    request_data = request.get_json()
+    for shop in shop_list:
+        if shop['name'] == shop_name:
+            new_item = {
+                'name' : request_data['name'],
+                'price' : request_data['price']
+            }
+            shop['items'].append(new_item)
+            return jsonify(new_item)
+    return jsonify({'message' : 'shop not found'})
 
 # GET  /shop/<string:shop_name>/item -> shop에 들어있는 item들의 목록을 반환
 @app.route('/shop/<string:shop_name>/item', methods=['GET'])
 def get_item_list_in_shop(shop_name):
-    pass
+    for shop in shop_list:
+        if shop['name'] == shop_name:
+            return jsonify({'items' : shop['items']})
+
 
 if __name__ == '__main__':
-    app.run()
+    app.run(debug=True)
